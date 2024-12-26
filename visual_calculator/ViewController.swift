75835f6b77a7ebb76e35b65bbc8d5dd7313df32a
Vadosmik <liontv05@gmail.com>
2024-12-26 01:10:44 +0100

    Initial Commit

---
+++ visual_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  visual_calculator
+//
+//  Created by Vadzim Mikanovich on 26/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ visual_calculatorTests/visual_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  visual_calculatorTests.swift
+//  visual_calculatorTests
+//
+//  Created by Vadzim Mikanovich on 26/12/2024.
+//
+
+import Testing
+@testable import visual_calculator
+
+struct visual_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
